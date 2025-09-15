140bff24ec054c1b0e68d48424ec991a88d36934
ajpelu <ajpelu@gmail.com>
2025-09-15 20:28:19 +0200

    update and fix people

--- scripts/generate_people_page.R
+++ scripts/generate_people_page.R
@@ -21,7 +21,7 @@ respuestas_fillout <- read_csv(file = file) |> janitor::clean_names()
 ## filter by date 
 respuestas_fillout<- respuestas_fillout |>
   mutate(last_updated2 = as.POSIXct(strptime(last_updated, "%a %b %d %Y %H:%M:%S", tz = "GMT"))) |> 
-  filter(last_updated2 >= as.POSIXct("2025-06-26", tz = "GMT"))
+  filter(last_updated2 >= as.POSIXct("2025-07-04", tz = "GMT"))
 
 respuestas_fillout |> unique()
 
